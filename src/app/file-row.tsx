b77b40b615911872c44aee0956e06b977507fd14
Miguel Mercedes <lmyguel0324@gmail.com>
2025-02-05 22:39:52 -0400

    Refactor DriveContents to use Link for navigation and simplify folder handling

--- src/app/file-row.tsx
+++ src/app/file-row.tsx
@@ -1,32 +1,6 @@
 import { FileIcon, FolderIcon } from "lucide-react";
+import Link from "next/link";
 import type { files, folders } from "~/server/db/schema";
-
-export function FolderRow(props: {
-  folder: typeof folders.$inferSelect;
-  handleFolderClick: () => void;
-}) {
-  const { folder, handleFolderClick } = props;
-  return (
-    <li
-      key={folder.id}
-      className="hover:bg-gray-750 border-b border-gray-700 px-6 py-4"
-    >
-      <div className="grid grid-cols-12 items-center gap-4">
-        <div className="col-span-6 flex items-center">
-          <button
-            onClick={() => handleFolderClick()}
-            className="flex items-center text-gray-100 hover:text-blue-400"
-          >
-            <FolderIcon className="mr-3" size={20} />
-            {folder.name}
-          </button>
-        </div>
-        <div className="col-span-3 text-gray-400"></div>
-        <div className="col-span-3 text-gray-400"></div>
-      </div>
-    </li>
-  );
-}
 
 export function FileRow(props: { file: typeof files.$inferSelect }) {
   const { file } = props;
@@ -49,6 +23,30 @@ export function FileRow(props: { file: typeof files.$inferSelect }) {
         </div>
         <div className="col-span-3 text-gray-400">{"file"}</div>
         <div className="col-span-3 text-gray-400">{file.size}</div>
+      </div>
+    </li>
+  );
+}
+
+export function FolderRow(props: { folder: typeof folders.$inferSelect }) {
+  const { folder } = props;
+  return (
+    <li
+      key={folder.id}
+      className="hover:bg-gray-750 border-b border-gray-700 px-6 py-4"
+    >
+      <div className="grid grid-cols-12 items-center gap-4">
+        <div className="col-span-6 flex items-center">
+          <Link
+            href={`/f/${folder.id}`}
+            className="flex items-center text-gray-100 hover:text-blue-400"
+          >
+            <FolderIcon className="mr-3" size={20} />
+            {folder.name}
+          </Link>
+        </div>
+        <div className="col-span-3 text-gray-400"></div>
+        <div className="col-span-3 text-gray-400"></div>
       </div>
     </li>
   );
