d379b5bb88aee5a2558c4056bdf23b71c168f2af
Miguel Mercedes <lmyguel0324@gmail.com>
2025-02-10 00:45:00 -0400

    Update .env

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -30,14 +30,16 @@ export const QUERIES = {
     return db
       .select()
       .from(folders_table)
-      .where(eq(folders_table.parent, folderId));
+      .where(eq(folders_table.parent, folderId))
+      .orderBy(folders_table.id);
   },
 
   getFiles: function (folderId: number) {
     return db
       .select()
       .from(files_table)
-      .where(eq(files_table.parent, folderId));
+      .where(eq(files_table.parent, folderId))
+      .orderBy(files_table.id);
   },
   getFolderById: async function (folderId: number) {
     const folder = await db
